66616f16f5482831cb1c3eb1aabdac0c2ecca986
Maryia Shynkarenka <maryia_shynkarenka@epam.com>
2017-08-03 14:56:02 +0300

    updated commit

--- src/test/java/TestUsers.java
+++ src/test/java/TestUsers.java
@@ -1,3 +1,4 @@
+import businessobjects.Users;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.testng.Assert;
@@ -7,7 +8,7 @@ import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
 
 
-public class Test1 {
+public class TestUsers {
 
     @BeforeTest
     public void initTest(){
@@ -16,8 +17,7 @@ public class Test1 {
 
     @Test
     public void checkStatusCode(){
-        Response rp = given().get("/posts").andReturn();
-        //given().headers();
+        Response rp = given().get("/users").andReturn();
         int actualStatusCode = rp.getStatusCode();
         System.out.println(actualStatusCode);
         Assert.assertEquals(actualStatusCode, 200);
@@ -25,15 +25,15 @@ public class Test1 {
 
     @Test
     public void checkResponseHeader(){
-        Response rp = given().get("/posts").andReturn();
+        Response rp = given().get("/users").andReturn();
         String valueOfContentTypeHeader = rp.getHeader("content-type");
         Assert.assertTrue(valueOfContentTypeHeader.contains("application/json"));
     }
 
     @Test
     public void checkResponseBody() {
-        Response rp = given().get("/posts").andReturn();
-        Post[] posts = rp.as(Post[].class);
-        Assert.assertEquals(posts.length, 100);
+        Response rp = given().get("/users").andReturn();
+        Users[] users = rp.as(Users[].class);
+        Assert.assertEquals(users.length, 10);
     }
 }
